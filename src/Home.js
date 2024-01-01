cbd7a6557057d558e70213dea9058dcfd7c46f9b
Pranjal Mahajan <pijal1503@gmail.com>
2024-01-01 19:40:11 +0530

    first commit

---
+++ src/Home.js
@@ -0,0 +1,19 @@
+import Movie from "./Movie";
+import Search from "./Search";
+import Test from "./Test"
+
+const Home = () => {
+  return (
+    <>
+      <div className="container">
+        
+        
+        <Search />
+        <Movie />
+        
+      </div>
+    </>
+  );
+};
+
+export default Home;
---
+++ src/context.js
@@ -0,0 +1,24 @@
+
+
+import React, { useContext, useState } from "react";
+import useFetch from "./useFetch";
+
+const AppContext = React.createContext();
+
+
+const AppProvider = ({ children }) => {
+  const [query, setQuery] = useState("titanic");
+  const { isLoading, isError, movie } = useFetch(`${query}`);
+
+  return (
+    <AppContext.Provider value={{ query, movie, setQuery, isLoading, isError }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider, useGlobalContext };
